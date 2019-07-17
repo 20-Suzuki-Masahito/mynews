a763e911614a14b3b4d875234a669a85033a77bd
”20_Suzuki_Masahito” <uppo1182@gmail.com>
2019-07-17 09:32:25 +0000

    PHP/Laravel_12

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,16 @@ Route::get('/', function () {
 
 // PHP/Laravel09 課題4
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 // PHP/Laravel09 課題3 *AAAControllerはAdminディレクトリ下に保存されているものとする
 Route::get('XXX', 'Admin\AAAController@bbb');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
